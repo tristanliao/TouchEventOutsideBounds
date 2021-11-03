e5195895b59c812c2e4f10cacc8dbb68c6c77180
TristanLiao <tristan@17.media>
2021-11-03 23:32:06 +0800

    Remove unnecessary code.

--- TouchEventOusideBounds/QuestionViewController.swift
+++ TouchEventOusideBounds/QuestionViewController.swift
@@ -23,15 +23,5 @@ class QuestionViewController: UIViewController {
         let labelString = "Tap count = \(tapCount)"
         tapCountLabel.text = labelString
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
